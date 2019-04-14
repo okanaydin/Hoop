b5679a5542bbb3c2269724dae613b0a91f8c245c
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-04-14 22:35:58 +0300

    view pager and tab layout added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

--- app/src/main/java/io/androidedu/hoop/ui/MainActivity.kt
+++ app/src/main/java/io/androidedu/hoop/ui/MainActivity.kt
@@ -1,13 +1,17 @@
 package io.androidedu.hoop.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import io.androidedu.hoop.R
 import io.androidedu.hoop.adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TabLayout.BaseOnTabSelectedListener<TabLayout.Tab>, ViewPager.OnPageChangeListener {
+
 
     private val chatsFragment by lazy { ChatsFragment.newInstance() }
     private val statusFragment by lazy { StatusFragment.newInstance() }
@@ -33,6 +37,38 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = ViewPagerAdapter(fragmentList, fragmentTitleList, supportFragmentManager)
 
         tabLayout.setupWithViewPager(viewPager)
+
+        viewPager.currentItem = 1
+
+        tabLayout.addOnTabSelectedListener(this)
+
+        viewPager.addOnPageChangeListener(this)
+
     }
+
+    override fun onTabReselected(p0: TabLayout.Tab?) {
+        Toast.makeText(this, "onTabReselected", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onTabUnselected(p0: TabLayout.Tab?) {
+        Toast.makeText(this, "onTabUnselected", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onTabSelected(p0: TabLayout.Tab?) {
+        Toast.makeText(this, "onTabSelected", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPageScrollStateChanged(state: Int) {
+        Toast.makeText(this, "onPageScrollStateChanged", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+        Toast.makeText(this, "onPageScrolled", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPageSelected(position: Int) {
+        Toast.makeText(this, "onPageSelected", Toast.LENGTH_SHORT).show()
+    }
+
 
 }
